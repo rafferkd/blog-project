2af56130915dd5f1616e2d22d9ce9c6d8386607c
rafferkd <rafferkd@gmail.com>
2017-12-21 21:54:07 -0500

    About page loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
-hat import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
@@ -10,7 +12,13 @@ import { AboutComponent } from './about/about.component';
     AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    RouterModule.forRoot([
+      { path: 'about', component: AboutComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
