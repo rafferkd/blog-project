6e23bb37271772459ca71c2ab48441179479f1cb
rafferkd <rafferkd@gmail.com>
2017-12-24 19:50:33 -0500

    Navigation changes and addiional pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,24 @@ import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
 import { AboutComponent } from './about/about.component';
+import { EventsComponent } from './events/events.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    AboutComponent
+    AboutComponent,
+    EventsComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
     RouterModule.forRoot([
       { path: 'about', component: AboutComponent },
+      { path: 'events', component: EventsComponent },
+      { path: 'login', component: LoginComponent },
       { path: '', redirectTo: 'about', pathMatch: 'full' },
       { path: '**', redirectTo: 'about', pathMatch: 'full' }
     ]),
